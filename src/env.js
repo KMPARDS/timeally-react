be9a4c7c5b4e63d5ca00aaf9bab572e5808a2404
zemse <sohamzemse143@gmail.com>
2019-07-23 19:20:56 +0530

    change contract

--- src/env.js
+++ src/env.js
@@ -5,15 +5,15 @@ const timeAllyJSON = require('./ethereum/compiledContracts/TimeAlly_0.json');
 module.exports = {
   network: 'kovan',
   esContract: {
-    address: '0x241A7aebE3b425Ee251eb06f3D00006B9509aBcB',
+    address: '0x8737812fCF6328E0ED8A2d39B5AfC54e9f23bFDe',
     abi: eraSwapTokenJSON.abi
   },
   nrtManager: {
-    address: '0x332D3Cb0071ac0FB71141dD15882304e31f2e2a4',
+    address: '0xb12575AD2d4954C45Da8Ec7A1c0993F4803a3c0c',
     abi: nrtManagerJSON.abi
   },
   timeally: {
-    address: '0x32af5428F0C0e5BDeceFe4F28617e0027DD69811',
+    address: '0xF90fF80d7312FAc992E3C18B57FFd07788DF5D78',
     abi: timeAllyJSON.abi
   }
 };
